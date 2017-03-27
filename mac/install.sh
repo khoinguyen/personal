f6fc07cf4a9045bbb2b1abf4962a46ce6b304d38
Khoi Nguyen <khoi.nqq@gmail.com>
2017-03-27 14:46:20 +0700

    install for new mac

---
+++ mac/install.sh
@@ -0,0 +1,16 @@
+echo "Install Homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Install Packages"
+brew install node
+#brew install 
+
+echo "Install Applications"
+brew cask install google-chrome
+brew cask install alfred
+brew cask install atom
+brew cask install 1password
+brew cask install slack
+brew cask install java
+brew cask install viber
+brew cask install dropbox
+
